9fd847c1fc994b2ee1c7b3239987bc2fc71c192e
wblackmon <wayne.blackmonl@outlook.com>
2025-07-26 17:33:45 -0500

    UI is functioning.

---
+++ AmountToWords.Web/Controllers/AmountController.cs
@@ -0,0 +1,25 @@
+﻿using AmountToWords.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmountToWords.Web.Controllers
+{
+    public class AmountController : Controller
+    {
+        [HttpPost]
+        public IActionResult Convert(AmountViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //model.AmountWords = NumberWords.ConvertAmountToWords(model.Amount);
+            }
+
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new AmountViewModel());
+        }
+    }
+}
---
+++ AmountToWords.Web/Models/AmountViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmountToWords.Web.Models
+{
+    public class AmountViewModel
+    {
+        [Required(ErrorMessage = "Please enter an amount")]
+        public decimal? Amount { get; set; } = null;
+        public  string? AmountWords { get; set; }
+    }
+}
